87a92261ff09229946f4f1bdea099a84a211ae22
Rassol1401 <rassolenkomaksim1@gmail.com>
2024-12-11 14:50:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from transliterate import translit 
+from num2words import num2words
+print()
+print()
 print(translit("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine.",'ru'))
 print(translit("People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies",'ru'))
 print(translit("Neither will happen.",'ru'))
 print()
 print(translit("More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...",'ru'))
 print()
-from num2words import num2words
 print(78, end="-")
 print(translit(num2words(78),"ru"))
 print(15, end="-")
